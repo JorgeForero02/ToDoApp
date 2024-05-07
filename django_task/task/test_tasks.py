69fcf220b5d194342661a132f1582852a5e19c6e
JorgeForero02 <forero07andres@gmail.com>
2024-05-07 09:30:12 -0500

    pruebas unitarias

--- django_task/task/test_tasks.py
+++ django_task/task/test_tasks.py
@@ -16,6 +16,13 @@ def test_tareaJson_view():
     assert response.status_code == 200
 
 @pytest.mark.django_db
+def test_tareaDetailJson_view():
+    client = Client()
+    tarea_id = 1
+    response = client.get(reverse("prueba:tareaDetail", kwargs={"tarea_id": tarea_id}))
+    assert response.status_code == 200
+
+@pytest.mark.django_db
 def test_tareaJson_orderList():
     client = Client()
     response = client.get(reverse("prueba:tareasJson"))
@@ -26,13 +33,4 @@ def test_tareaJson_orderList():
 
     prioridades = [tarea["priority"] for tarea in latest_task_list]
     assert prioridades == sorted(prioridades, reverse=True)
-
-@pytest.mark.django_db
-def test_tareaDetailJson_view():
-    client = Client()
-    tarea_id = 1
-    response = client.get(reverse("prueba:tareaDetail", kwargs={"tarea_id": tarea_id}))
-    assert response.status_code == 200
-
-
 
